dfbf1b1534bb003e3335040d208355c5a7d32ae0
DESKTOP-28L3Q6V\82103 <ssin011123@gamil.com>
2023-09-05 12:05:49 +0900

    Chore : Build Test

--- NetworkTraining/Assets/Shin/Scripts/CameraControll.cs
+++ NetworkTraining/Assets/Shin/Scripts/CameraControll.cs
@@ -2,6 +2,7 @@ using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraControll : MonoBehaviour
 {
@@ -20,19 +21,19 @@ public class CameraControll : MonoBehaviour
             // 마우스 입력값 받기
 
             // 마우스의 수직 값 상승 == 카메라의 수직축 하강으로 이어짐 => 값을 인버스해서 대입해준다.
-            float mouseV = -Input.GetAxis("Mouse Y") * 500f * Time.deltaTime;
-            float mouseH = Input.GetAxis("Mouse X") * 500f * Time.deltaTime;
+            Vector2 mouseMove = new Vector2(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X")) * 500f * Time.deltaTime;
+            Vector3 camera = transform.rotation.eulerAngles;
 
-            mouseV = Mathf.Clamp(mouseV, -90f, 90f);
+            mouseMove.x = Mathf.Clamp(mouseMove.x, -90f, 90f);
 
+
+            transform.rotation = Quaternion.Euler(camera.x + mouseMove.x, camera.y + mouseMove.y, camera.z);
+
+            Debug.DrawRay(transform.position, transform.forward * 30f, Color.yellow);
             // 카메라 수평 회전 ( 카메라 Y 로테이트 값 )
-            transform.RotateAround(transform.parent.position, Vector3.up, mouseH);
-
-            //카메라 수직 회전 ( 카메라 X 로테이트 값 )
-            transform.RotateAround(transform.parent.position, Vector3.right, mouseV);
-
-            // 최종적으로 캐릭터를 바라본다.
-            transform.LookAt(new Vector3(transform.parent.position.x -0.5f, transform.parent.position.y + 1f, transform.parent.position.z));
+            
         }
+
+        transform.position = transform.parent.GetChild(0).transform.position;
     }
 }
--- NetworkTraining/Assets/Shin/Scripts/CameraSetup.cs
+++ NetworkTraining/Assets/Shin/Scripts/CameraSetup.cs
@@ -9,16 +9,27 @@ using UnityChan;
 
 public class CameraSetup : MonoBehaviourPun
 {
+    //private PhotonView pv;
+    private CinemachineVirtualCamera playerCam;
+    Transform camerafoward;
+
+    private Vector3 playerPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        // 만약 자신이 로컬 플레이어라면
-        if(photonView.IsMine)
-        {
-            // 씬에 있는 시네머신 가상 카메라를 찾고           
-            Camera.main.transform.position = transform.GetChild(3).transform.position;
+        playerCam = FindObjectOfType<CinemachineVirtualCamera>();
+        playerPos = transform.GetChild(0).transform.position;
+        camerafoward = playerCam.transform;
+        //pv = GetComponent<PhotonView>();
 
-            Camera.main.transform.parent = transform.GetChild(3).transform;
+        // 만약 자신이 로컬 플레이어라면
+        if (photonView.IsMine)
+        {
+            // 씬에 있는 시네머신 가상 카메라를 찾고
+
+            playerCam.transform.position = new Vector3(playerPos.x, playerPos.y + 10f, playerPos.z - 8f);
+            playerCam.transform.parent = transform.GetChild(1).transform;
 
             //GameObject.Find("Camera").GetComponent<Camera>();
             //CameraControll follow = new CameraControll();
@@ -26,7 +37,7 @@ public class CameraSetup : MonoBehaviourPun
             //follow.FollowCam = transform.GetChild(3).GetComponent<GameObject>();
 
             //Debug.Log(transform.GetChild(3).name);
-            
+
             // 자신을 바라보며 따라오게 한다.
             //followCam.Follow = transform;
             //followCam.LookAt = transform;
